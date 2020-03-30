56d02c03e292b4108856af0ff855a9e821b69a9a
joeljhanster <50553469+joeljhanster@users.noreply.github.com>
2020-03-31 00:12:11 +0800

    added viewjournal command

--- main.py
+++ main.py
@@ -46,21 +46,24 @@ def caption(update, context):
 def cancel(update, context):
     user = update.message.from_user
     logger.info("User %s canceled the conversation.", user.first_name)
-    update.message.reply_text('Bye! I hope we can talk again some day.',
-                              reply_markup=ReplyKeyboardRemove())
-
+    update.message.reply_text('Bye! I hope we can talk again some day.', reply_markup=ReplyKeyboardRemove())
     return ConversationHandler.END
+
+def viewjournal(update, context):
+    # Opens up the blogger website for browsing
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Here's where all the memories are stored:\nhttps://youcalledmewithyourheart.blogspot.com/")
 
 def daily_encouragement(context):
     timedelta = datetime.date.today() - day0
     diff_days = timedelta.days
-    # Edit message here using random generator
+    ### TODO: AUTO-GENERATE MESSAGES TO BE SHARED DAILY, CAN BE BIBLE VERSES, QOTD, LOVE MESSAGES, WORDS OF ENCOURAGEMENT ###
     for id in chatId:
         context.bot.send_message(chat_id=id, text="Day {}: I love you".format(diff_days))
 
 def unknown(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="You are loved! Maybe you want to type another command? :)")
 
+### TODO: REMOVE THIS FUNCTION ###
 def callback_minute(context):
     now = datetime.datetime.now()
     current_time = now.strftime("%H:%M:%S")
@@ -84,10 +87,14 @@ def main():
     )
     dispatcher.add_handler(journal_handler)
 
+    viewjournal_handler = CommandHandler('viewjournal', viewjournal)
+    dispatcher.add_handler(viewjournal_handler)
+
     unknown_handler = MessageHandler(Filters.command, unknown)
     dispatcher.add_handler(unknown_handler)
 
     # JOB QUEUE
+    ### TODO: CHECK WHETHER THE REMINDER IS SET CORRECTLY ###
     job = updater.job_queue
     job.run_daily(daily_encouragement, time = datetime.time(20,8,5,5))
     # print(job.jobs())
@@ -95,6 +102,7 @@ def main():
     # j.run_repeating(callback_minute, interval=5, first=0)
     # print(j.jobs())
 
+    ### TODO: MAKE THE TELEGRAM BOT PERSISTENT ###
     print("Starting telegram bot")
     updater.start_polling()
     updater.idle()
