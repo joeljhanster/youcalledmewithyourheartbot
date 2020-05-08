786cc08d993774c71b022ad12d72755f0ccf5343
joeljhanster <50553469+joeljhanster@users.noreply.github.com>
2020-05-08 11:06:47 +0800

    test

--- main.py
+++ main.py
@@ -98,10 +98,10 @@ def word(update, context):
             context.bot.send_message(chat_id=id, text=received_message)
             context.bot.send_message(chat_id=id, text=update.message.text)
         else:
-            received_message = emojize("Your partner has a word of encouragement for you! Remember to show your appreciation! :kissing_heart:\n\n{0}".format(update.message.text), use_aliases=True)   #testing
+            received_message = emojize("Your partner has a word of encouragement for you! Remember to show your appreciation! :kissing_heart:", use_aliases=True)   #testing
             context.bot.send_message(chat_id=HAN_ID, text=received_message)    # testing
-            # context.bot.send_message(chat_id=HAN_ID, text=update.message.text) # testing
-            sent_message = emojize("Your partner should have received your word of encouragement! :+1:", use_aliases=True)
+            context.bot.send_message(chat_id=HAN_ID, text=update.message.text) # testing
+            # sent_message = emojize("Your partner should have received your word of encouragement! :+1:", use_aliases=True)
             context.bot.send_message(chat_id=id, text=sent_message)
     return ConversationHandler.END
 
